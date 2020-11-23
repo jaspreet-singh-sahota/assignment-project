682bf630dbed8a14846f64b4493be6220a9779dd
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-23 09:46:34 +0530

    Add Navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './styles.module.css'
+import Button from '../button/Button'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles['nav-items']}>
+        <div className={styles['nav-item']}>R logo</div>
+        <div className={styles['nav-item']}>Individuals</div>
+        <div className={styles['nav-item']}>Tech Employers</div>
+        <div className={styles['nav-item']}>Property Managers</div>
+        <div className={styles['nav-item']}>Gig Economies</div>
+        <div className={styles['nav-item']}>Social Media Check</div>
+        <div className={styles['nav-item']}>How It Works</div>
+        <div className={styles['nav-item']}>About</div>
+        <div className={styles['nav-item']}>Contact</div>
+      </div>
+      <div className={styles['nav-items']}>
+        <div className={styles.button}>
+          <Button inverted type={'button'}>Login</Button>
+        </div>
+        <div className={styles.button}>
+          <Button type={'button'}>Live Demo</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
