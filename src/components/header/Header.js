dc93a14d12d6ff238e83e67d6f1d246448be9083
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-24 16:59:23 +0530

    Add Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Your most valuable asset is the number
+            of people who know, like & trust you
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
