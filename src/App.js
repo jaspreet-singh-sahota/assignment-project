794504a453329bb3cd6548d598771b1fdb22bcf3
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-24 17:04:55 +0530

    Add Navbar collapsing functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <HomePage/>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
 import Button from '../button/Button'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { ImCross } from 'react-icons/im';
 
 const Navbar = () => {
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      window.innerWidth > 1220 ? setShowHamburgerMenu(false) : setShowHamburgerMenu(true);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles['nav-items']}>
-        <div className={styles['nav-item']}>R logo</div>
-        <div className={styles['nav-item']}>Individuals</div>
-        <div className={styles['nav-item']}>Tech Employers</div>
-        <div className={styles['nav-item']}>Property Managers</div>
-        <div className={styles['nav-item']}>Gig Economies</div>
-        <div className={styles['nav-item']}>Social Media Check</div>
-        <div className={styles['nav-item']}>How It Works</div>
-        <div className={styles['nav-item']}>About</div>
-        <div className={styles['nav-item']}>Contact</div>
+      <div className={styles['left-nav-items']}>
+        <div className={styles.logo}>R logo</div>
+        {showHamburgerMenu ? !isMenuOpen ?
+          <GiHamburgerMenu
+            onClick={() => setIsMenuOpen(prev => !prev)}
+            className={styles['hamburger-menu']}
+          /> :
+          <ImCross
+            onClick={() => setIsMenuOpen(prev => !prev)}
+            className={styles['cross-icon']}
+          /> : null
+        }
+        {
+          (isMenuOpen || !showHamburgerMenu) &&
+          <div className={styles[!showHamburgerMenu ? 'nav-items' : 'sidebar-items']}>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Individuals</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Tech Employers</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Property Managers</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Gig Economies</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Social Media Check</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>How It Works</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>About</div>
+            <div className={styles[!showHamburgerMenu ? 'nav-item' : 'sidebar-item']}>Contact</div>
+          </div>
+        }
       </div>
       <div className={styles['nav-items']}>
         <div className={styles.button}>
