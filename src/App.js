8dd47c970289fc50bc7b68a1639eba571d8ea5e2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-25 21:30:36 +0530

    Add react-router to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
 import Navbar from './components/navbar/Navbar';
 import HomePage from './pages/homepage/HomePage';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <HomePage/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
