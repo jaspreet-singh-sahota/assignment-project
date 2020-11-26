d98d48ad4563b04e20361875e7501f904fe65183
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-26 18:49:44 +0530

    Refactor data in DetailCardComponent

--- src/components/detail-card-row/DetailCardRow.js
+++ src/components/detail-card-row/DetailCardRow.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import DetailCard from '../detail-card/DetailCard'
 import styles from './styles.module.css'
-import { BsFillLockFill } from 'react-icons/bs'
-import { TiTick } from 'react-icons/ti'
-import { FaUserSecret } from 'react-icons/fa'
+import {cardData} from './cardData'
 
 const DetailCardRow = () => {
   return (
     <div className={styles['row-container']}>
-      <DetailCard
-        icon={<BsFillLockFill/>}
-        title={'Where your reputation lives'}
-        info={'Secure, own and control your reputation from any service.'}
-      />
-      <DetailCard
-        icon={<TiTick/>}
-        title={'Trust and interact with confidence'}
-        info={'Verify you can trust a stranger, in any interaction, online or face to face.'}
-      />
-      <DetailCard
-        icon={<FaUserSecret/>}
-        title={'Anonymous yet trusted'}
-        info={'Prove to others you are trustworthy (e.g. when applying for a job, home rental or loan), without giving them access to your profiles.'}
-      />
+      {cardData.map(data =>
+        <DetailCard key={data.id}
+          icon={<data.icon />}
+          title={data.title}
+          info={data.info}
+        />)   
+      }
     </div>
   )
 }
